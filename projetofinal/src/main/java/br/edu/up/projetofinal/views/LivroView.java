01c20cade620065e57f9bb32a6cf97ea332fea40
joao-moorgado <joao.moorgado@gmail.com>
2024-06-24 20:29:25 -0300

    Resolucao erros LivroView

--- projetofinal/src/main/java/br/edu/up/projetofinal/views/LivroView.java
+++ projetofinal/src/main/java/br/edu/up/projetofinal/views/LivroView.java
@@ -43,104 +43,104 @@ public class LivroView {
             case 99 -> Util.showFeedbackMessage("Informe um valor inteiro.");
             default -> Util.showFeedbackMessage("Opção invalida! Favor escolher opção existente no menu.");
         }
+    }
 
-        private static void cadastrar(Scanner scanner) {
-            try {
-                System.out.println("Digite o titulo: ");
-                var titulo = scanner.nextLine();
+    private static void cadastrar(Scanner scanner) {
+        try {
+            System.out.println("Digite o titulo: ");
+            var titulo = scanner.nextLine();
 
-                System.out.println("Digite o autor: ");
-                var autor = scanner.nextLine();
+            System.out.println("Digite o autor: ");
+            var autor = scanner.nextLine();
 
-                System.out.println("Digite o gênero: ");
-                var genero = scanner.nextLine();
+            System.out.println("Digite o gênero: ");
+            var genero = scanner.nextLine();
 
-                System.out.println("Digite a editora: ");
-                var editora = scanner.nextLine();
+            System.out.println("Digite a editora: ");
+            var editora = scanner.nextLine();
 
-                /*Aqui a gente pode fazer igual com a opção de mostrar os dados
-                do genero/autor/editora para dai selecionar por uuid:
+            /*Aqui a gente pode fazer igual com a opção de mostrar os dados
+            do genero/autor/editora para dai selecionar por uuid:
 
-                UsuarioView.exibirDadosUsuarios();
-                System.out.println("Escolha o usuario por UUID: ");
-                var uuid = scanner.nextLine();
+            UsuarioView.exibirDadosUsuarios();
+            System.out.println("Escolha o usuario por UUID: ");
+            var uuid = scanner.nextLine();
 
-                // Buscando os dados do usuario
-                var usuario = UsuarioController.buscarUsuarioPorUUID(UUID.fromString(uuid));*/
+            // Buscando os dados do usuario
+            var usuario = UsuarioController.buscarUsuarioPorUUID(UUID.fromString(uuid));*/
 
-                // criando o objeto
-                var livro = new Livro(titulo, autor, genero, editora);
+            // criando o objeto
+            var livro = new Livro(titulo, autor, genero, editora);
 
-                //salvando o objeto tarefa
-                LivroController.cadastrar(livro);
+            //salvando o objeto tarefa
+            LivroController.cadastrar(livro);
 
-            } catch (Exception ex) {
-                logger.error("Ocorreu um erro ao tentar cadastrar um livro.", ex);
-            }
+        } catch (Exception ex) {
+            logger.error("Ocorreu um erro ao tentar cadastrar um livro.", ex);
         }
+    }
 
 
-        private static void atualizar(Scanner scanner) {
-            try {
-                listar();
-                System.out.println("Qual Livro você deseja atualizar?");
-                var uuid = scanner.nextLine();
+    private static void atualizar(Scanner scanner) {
+        try {
+            listar();
+            System.out.println("Qual Livro você deseja atualizar?");
+            var uuid = scanner.nextLine();
 
-                System.out.println("####################################");
-                System.out.println("ATUALIZAÇÃO");
-                System.out.println("####################################");
+            System.out.println("####################################");
+            System.out.println("ATUALIZAÇÃO");
+            System.out.println("####################################");
 
-                System.out.println("Digite o título: ");
-                var titulo = scanner.nextLine();
+            System.out.println("Digite o título: ");
+            var titulo = scanner.nextLine();
 
-                System.out.println("Digite o autor: ");
-                var autor = scanner.nextLine();
+            System.out.println("Digite o autor: ");
+            var autor = scanner.nextLine();
 
-                System.out.println("Digite o gênero: ");
-                var genero = scanner.nextLine();
+            System.out.println("Digite o gênero: ");
+            var genero = scanner.nextLine();
 
-                System.out.println("Digite a editora: ");
-                var editora = scanner.nextLine();
+            System.out.println("Digite a editora: ");
+            var editora = scanner.nextLine();
 
-                // criando o objeto
-                var livro = new Livro();
-                livro.setTitulo(titulo);
-                livro.setAutor(autor);
-                livro.setGenero(genero);
-                livro.setEditora(editora);
+            // criando o objeto
+            var livro = new Livro();
+            livro.setTitulo(titulo);
+            livro.setAutor(autor);
+            livro.setGenero(genero);
+            livro.setEditora(editora);
 
-                //salvando o objeto tarefa
-                TarefaController.atualizar(UUID.fromString(uuid), tarefa);
-            } catch (TarefaNotFoundException ex) {
-                Util.showFeedbackMessage(ex.getMessage());
-                logger.warn("Ocorreu um erro ao tentar atualizar a tarefa.", ex);
-            } catch (Exception ex) {
-                var message = "Ocorreu um erro ao tentar criar uma tarefa.";
-                Util.showFeedbackMessage(message);
-                logger.error(message, ex);
-            }
+            //salvando o objeto tarefa
+            TarefaController.atualizar(UUID.fromString(uuid), tarefa);
+        } catch (TarefaNotFoundException ex) {
+            Util.showFeedbackMessage(ex.getMessage());
+            logger.warn("Ocorreu um erro ao tentar atualizar a tarefa.", ex);
+        } catch (Exception ex) {
+            var message = "Ocorreu um erro ao tentar criar uma tarefa.";
+            Util.showFeedbackMessage(message);
+            logger.error(message, ex);
         }
+    }
 
-        private static void remover(Scanner scanner) {
-            try {
-                listar();
-                System.out.println("Qual Tarefa você deseja remover? ");
-                var uuid = scanner.nextLine();
+    private static void remover(Scanner scanner) {
+        try {
+            listar();
+            System.out.println("Qual Tarefa você deseja remover? ");
+            var uuid = scanner.nextLine();
 
-                TarefaController.remover(UUID.fromString(uuid));
-            } catch (LivroNotFoundException ex) {
-                Util.showFeedbackMessage(ex.getMessage(), true);
-                logger.warn("Ocorreu um erro ao tentar remover o livro.", ex);
-            }
+            TarefaController.remover(UUID.fromString(uuid));
+        } catch (LivroNotFoundException ex) {
+            Util.showFeedbackMessage(ex.getMessage(), true);
+            logger.warn("Ocorreu um erro ao tentar remover o livro.", ex);
         }
+    }
 
-        private static void listar() {
-            var livros = LivroController.listar();
-            System.out.println("######## LISTA DE LIVROS ############");
-            livros.forEach(livro -> {
-                exibirDadosLivro(livro, false);
-            });
-            System.out.println("########################################");
-        }
+    private static void listar() {
+        var livros = LivroController.listar();
+        System.out.println("######## LISTA DE LIVROS ############");
+        livros.forEach(livro -> {
+            exibirDadosLivro(livro, false);
+        });
+        System.out.println("########################################");
     }
 }
